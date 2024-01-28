00418fa290c036a8e4f3d3d413f95e94c95eb843
Abduraimov <abduraim0vnurtilek26@gmail.com>
2024-01-28 16:57:54 +0600

    Initial Commit

---
+++ my real project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my real project 1
+//
+//  Created by Nurtilek on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
